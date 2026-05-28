9504b03ada05b778603c849cf0a2144e8c3c96ae
Dianochka <stoljarova2003@yandex.ru>
2026-05-28 12:20:58 +0300

    extract the texts of all objects

--- find.py
+++ find.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.by import By
 url = "http://parsinger.ru/selenium/3/3.html"
 with webdriver.Chrome() as browser:
     browser.get(url)
-    link = browser.find_element(By.CLASS_NAME, 'text')
-    print(link.text)
+    links = browser.find_elements(By.CLASS_NAME, 'text')
+    print(links)
