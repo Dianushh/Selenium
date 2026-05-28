185910553fbfabf0bb5b2bc2b383f76a7457a39c
Dianochka <stoljarova2003@yandex.ru>
2026-05-28 12:02:18 +0300

    Извлечь атрибут найденной кнопки

--- description.py
+++ description.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-browser = webdriver.Chrom()
-browser.get("https://parsinger.ru/html/watch/1/1_5.html")
-product = browser.find_element(By.CLASS_NAME, "description")
 
-data = product.text
 
-print(data)
+browser = webdriver.Chrome()
+browser.get("https://parsinger.ru/selenium/3/3.2.4/index.html")
+button = browser.find_element(By.ID, "secret-key-button").click()
+elem = browser.find_element(By.ID, "secret-key-button").get_attribute("data")
+
+
+time.sleep(10)
+
+print(elem)
+time.sleep(10)
+
 browser.quit()
-
-
